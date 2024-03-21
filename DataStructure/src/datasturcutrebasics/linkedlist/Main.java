b663ba19b0b19f0e9b4a30a79d36f5800b9d97f1
gpl31.msh@gmail.com <gpl31.msh@gmail.com>
2024-03-21 22:58:59 +0530

    Datastructure linked list session 2

--- DataStructure/src/datasturcutrebasics/linkedlist/Main.java
+++ DataStructure/src/datasturcutrebasics/linkedlist/Main.java
@@ -4,6 +4,51 @@ public class Main
 {
     public static void main(String args[])
     {
-        LinkedList mylinkedList = new LinkedList(4);
+//        LinkedList mylinkedList = new LinkedList(2);
+
+/*        mylinkedList.getHead();
+        mylinkedList.getTail();
+        mylinkedList.getLength();*/
+
+//        mylinkedList.append(1);
+//        //(2) Items - Return 2 Node
+//        System.out.println(mylinkedList.removeLast().value);
+//        //(1) Items - Return 2 Node
+//        System.out.println(mylinkedList.removeLast().value);
+//        //(0) Items - Return 2 Node
+//        System.out.println(mylinkedList.removeLast());
+
+        //mylinkedList.prepend(1);
+        //mylinkedList.printList();
+
+          //(2) Items - Return 2 Node
+//        System.out.println(mylinkedList.removeFirst().value);
+//        //(1) Items - Return 1 Node
+//        System.out.println(mylinkedList.removeFirst().value);
+//        //(0) Items - Returns null
+//        System.out.println(mylinkedList.removeFirst());
+
+//        LinkedList myLinkedList = new LinkedList(0);
+//        myLinkedList.append(1);
+//        myLinkedList.append(2);
+//        myLinkedList.append(3);
+//
+//        System.out.println(myLinkedList.get(2).value +"\n");
+//        myLinkedList.printList();
+//        LinkedList mylinkedList = new LinkedList(11);
+//        mylinkedList.append(3);
+//        mylinkedList.append(23);
+//        mylinkedList.append(7);
+//        mylinkedList.set(1,4);
+//
+//        mylinkedList.printList();
+
+        LinkedList mylinkedList = new LinkedList(0);
+        mylinkedList.append(2);
+
+        mylinkedList.insert(1,1);
+
+        mylinkedList.printList();
+
     }
 }
